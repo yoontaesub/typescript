bfa44ec159ac27f78e7ed6ed0e4a792b19d222d0
yoontaesub <yoontaesub@gmail.com>
2023-06-22 17:56:30 +0900

    test2

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@ interface User {
 
 const user: User = {
   name: '포도빛',
-  age: 20, // 오류가 발생해야 함!@
+  age: 20, // 오류가 발생해야 함!@#$%
   site: {
     blog: 'https://phodobit.kr',
     github: 'https://github.com/smartse0k',
